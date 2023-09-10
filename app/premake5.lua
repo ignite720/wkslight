0e393e73b8d8e260d0864efcc2b0f5e81eb3bc62
U-WIN-6O36QGINL6D\abcgg <ignite720@outlook.com>
2023-09-10 19:25:42 +0800

    init11

--- app/premake5.lua
+++ app/premake5.lua
@@ -16,5 +16,4 @@ project("app")
 		"bar",
 		"foo",
 		"linmath",
-		--"TGUI",
 	})
--- app/src/Main.cpp
+++ app/src/Main.cpp
@@ -3,30 +3,28 @@
 
 #include <linmath.h>
 
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
     {
-        {
-            foo_print(10.0);
-            foo_printi(20);
-            foo_printi64(30000123456789);
+        foo_print(10.0);
+        foo_printi(20);
+        foo_printi64(30000123456789);
 
-            Foo foo;
-            foo.print(40);
-        }
+        Foo foo;
+        foo.print(40);
+    }
 
-        {
-            Bar<int> bar;
-            bar.print(100);
+    {
+        Bar<int> bar;
+        bar.print(100);
 
-            Bar<float> bar2;
-            bar2.print(101.0f);
+        Bar<float> bar2;
+        bar2.print(101.0f);
 
 #if 0
-            Bar<double> bar3; // LNK2019
-            bar3.print(201.0);
+        Bar<double> bar3; // LNK2019
+        bar3.print(201.0);
 #endif
-        }
     }
 
     {
