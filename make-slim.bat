b19679f774956ce0045e90f02a26998ab073699b
ignite720 <ignite720@outlook.com>
2024-03-05 23:45:32 +0800

    init25

---
+++ make-slim.bat
@@ -0,0 +1,9 @@
+@echo off
+
+rd /s /q .git
+rd /s /q bin
+rd /s /q build
+
+rd /s /q "premake5-modules\android-studio\.github"
+rd /s /q "premake5-modules\android-studio\example"
+del /a /f /q "premake5-modules\android-studio\premake5"
