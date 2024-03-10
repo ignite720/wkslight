10b41d849525afc4ac60bc527fc5a140d9faf698
ignite720 <ignite720@users.noreply.github.com>
2024-03-10 15:12:56 +0000

    Deploying to gh-pages from @ ignite720/wkslight@f5626b770f1dc390004a4e6142f6907c82b967b6 🚀

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var Module = typeof Module != 'undefined' ? Module : {};
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmp390ooyji.js
+// include: /tmp/tmpo6st6gva.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -196,21 +196,21 @@ Module['FS_createPath']("/app", "assets", true, true);
 
   })();
 
-// end include: /tmp/tmp390ooyji.js
-// include: /tmp/tmp9om8a4s7.js
+// end include: /tmp/tmpo6st6gva.js
+// include: /tmp/tmpgosx9h2z.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['ENVIRONMENT_IS_PTHREAD'] || Module['$ww']) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp9om8a4s7.js
-// include: /tmp/tmpqobk4qnv.js
+  // end include: /tmp/tmpgosx9h2z.js
+// include: /tmp/tmpp1p73noi.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpqobk4qnv.js
+  // end include: /tmp/tmpp1p73noi.js
 
 
 // Sometimes an existing Module object exists with properties
@@ -12005,8 +12005,8 @@ var wasmImports = {
 var wasmExports = createWasm();
 var ___wasm_call_ctors = createExportWrapper('__wasm_call_ctors');
 var _main = Module['_main'] = createExportWrapper('__main_argc_argv');
-var _malloc = createExportWrapper('malloc');
 var _free = createExportWrapper('free');
+var _malloc = createExportWrapper('malloc');
 var setTempRet0 = createExportWrapper('setTempRet0');
 var _fflush = createExportWrapper('fflush');
 var _fileno = createExportWrapper('fileno');
