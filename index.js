16e58d6c0b001522ed66a4306621466164380073
ignite720 <132559332+ignite720@users.noreply.github.com>
2024-03-11 15:54:45 +0000

    Deploying to gh-pages from @ ignite720/wkslight@8e164db53b5b19135a35a34cb31748e8dca0a2d7 🚀

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ var Module = typeof Module != 'undefined' ? Module : {};
 
 // --pre-jses are emitted after the Module integration code, so that they can
 // refer to Module (if they choose; they can also define Module)
-// include: /tmp/tmpm1s7vxux.js
+// include: /tmp/tmpxc7ib634.js
 
   if (!Module.expectedDataFileDownloads) {
     Module.expectedDataFileDownloads = 0;
@@ -196,21 +196,21 @@ Module['FS_createPath']("/app", "assets", true, true);
 
   })();
 
-// end include: /tmp/tmpm1s7vxux.js
-// include: /tmp/tmp25fda75s.js
+// end include: /tmp/tmpxc7ib634.js
+// include: /tmp/tmpa1jljj7z.js
 
     // All the pre-js content up to here must remain later on, we need to run
     // it.
     if (Module['ENVIRONMENT_IS_PTHREAD'] || Module['$ww']) Module['preRun'] = [];
     var necessaryPreJSTasks = Module['preRun'].slice();
-  // end include: /tmp/tmp25fda75s.js
-// include: /tmp/tmpo7hps7bv.js
+  // end include: /tmp/tmpa1jljj7z.js
+// include: /tmp/tmpd3h_30rb.js
 
     if (!Module['preRun']) throw 'Module.preRun should exist because file support used it; did a pre-js delete it?';
     necessaryPreJSTasks.forEach(function(task) {
       if (Module['preRun'].indexOf(task) < 0) throw 'All preRun tasks that exist before user pre-js code should remain after; did you replace Module or modify Module.preRun?';
     });
-  // end include: /tmp/tmpo7hps7bv.js
+  // end include: /tmp/tmpd3h_30rb.js
 
 
 // Sometimes an existing Module object exists with properties
