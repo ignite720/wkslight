58e4ad20f28c374363d01b68da7642dafb8e235e
ignite720 <ignite720@outlook.com>
2024-12-13 01:34:36 +0800

    init54

--- scripts/24-gmake-web.sh
+++ scripts/24-gmake-web.sh
@@ -2,5 +2,5 @@ pushd build
 make config=release_wasm
 popd
 
-cp -fv app/meta/web/favicon.ico bin
-echo -e '{ "name": "Alexander", "age": 26, "address": { "city": "Wonderland", "zipcode": "54321" } }' > bin/example.json
+cp -fv app/meta/web/favicon.ico bin/target
+echo -e '{ "name": "Alexander", "age": 26, "address": { "city": "Wonderland", "zipcode": "54321" } }' > bin/target/example.json
