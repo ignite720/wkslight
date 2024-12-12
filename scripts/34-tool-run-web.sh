f347d5f3d5166c5fd78fff339f23c0a387fe0f31
ignite720 <ignite720@outlook.com>
2024-12-13 01:29:14 +0800

    init52

--- scripts/34-tool-run-web.sh
+++ scripts/34-tool-run-web.sh
@@ -3,6 +3,6 @@ echo $EMSDK_TMP_CACHE_SYSROOT_INC_PATH
 
 #export C_INCLUDE_PATH=${EMSDK_TMP_CACHE_SYSROOT_INC_PATH}
 #export CPLUS_INCLUDE_PATH=${EMSDK_TMP_CACHE_SYSROOT_INC_PATH}
-source gmake-web.sh
+source 24-gmake-web.sh
 
 emrun --no_browser --port=8000 ./bin
