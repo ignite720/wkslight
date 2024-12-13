7cb07827661c8b2170c7bce7d9afa2f143c3a3ad
ignite720 <ignite720@outlook.com>
2024-12-14 02:20:33 +0800

    init93

--- libraries/app_core/src/impl/platform/web/web_prelude.h
+++ libraries/app_core/src/impl/platform/web/web_prelude.h
@@ -8,34 +8,9 @@
 
 #endif
 
-#if 1
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_mixer.h>
-#else
-// todo: temporary test code
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
-
-struct SDL_FPoint {
-    float x;
-    float y;
-};
-
-struct SDL_FRect {
-    float x;
-    float y;
-    float w;
-    float h;
-};
-
-SDL_bool SDL_HasIntersectionF(const SDL_FRect * A, const SDL_FRect * B) {
-    auto a = SDL_Rect { int(A->x), int(A->y), int(A->w), int(A->h) };
-    auto b = SDL_Rect { int(B->x), int(B->y), int(B->w), int(B->h) };
-    return SDL_HasIntersection(&a, &b);
-}
-#endif
 
 enum MOVE_STATE {
     MOVE_STATE_NONE = 0,
