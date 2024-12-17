ca5f6f0e08c7d93a70fe5c446defba8e022a427b
ignite720 <ignite720@outlook.com>
2024-12-18 01:12:28 +0800

    init187

--- libraries/app_core/src/impl/platform/web/actor.cpp
+++ libraries/app_core/src/impl/platform/web/actor.cpp
@@ -9,11 +9,13 @@ Actor::Actor(AppCore *app_core, float x, float y)
 }
 
 void Actor::update(float dt) {
-    m_dst_rect.x += m_velocity.x;
-    m_dst_rect.y += m_velocity.y;
-
-    if (m_dead && utils::sdl::now() >= m_respawn_time) {
-        this->on_spawn(dt);
+    if (m_dead) {
+        if (utils::sdl::now() >= m_respawn_time) {
+            this->on_spawn(dt);
+        }
+    } else {
+        m_dst_rect.x += m_velocity.x;
+        m_dst_rect.y += m_velocity.y;
     }
 }
 
