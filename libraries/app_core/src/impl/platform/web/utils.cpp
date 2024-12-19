13dcbc25b169025e57a919f56f93dc1d10e6c9b5
ignite720 <ignite720@outlook.com>
2024-12-19 21:42:03 +0800

    init223

--- libraries/app_core/src/impl/platform/web/utils.cpp
+++ libraries/app_core/src/impl/platform/web/utils.cpp
@@ -7,11 +7,25 @@ static constexpr const char *const REQUEST_HEADERS[] = {
     nullptr
 };
 
+struct WebFetchUserData {
+    void *data = nullptr;
+    size_t size = 0;
+};
+
 static void s_web_fetch_succeeded(emscripten_fetch_t *fetch) {
     if (fetch->data && (fetch->numBytes > 0)) {
         const auto symbol = utils::string::str_repeats("=", 98);
-        const auto text = String { fetch->data, fetch->data + fetch->numBytes };
-        printf("%s>>\nweb_fetch: %s\n%s\n<<%s\n\n", symbol.c_str(), fetch->url, text.c_str(), symbol.c_str());
+
+        auto *user_data = static_cast<WebFetchUserData *>(fetch->userData);
+        if (user_data) {
+            assert(user_data->size == fetch->numBytes);
+            memcpy(user_data->data, fetch->data, fetch->numBytes);
+
+            printf("%s>>\nweb_fetch: %s\nfetch status => %d\n<<%s\n\n", symbol.c_str(), fetch->url, fetch->status, symbol.c_str());
+        } else {
+            const auto text = String { fetch->data, fetch->data + fetch->numBytes };
+            printf("%s>>\nweb_fetch: %s\n%s\n<<%s\n\n", symbol.c_str(), fetch->url, text.c_str(), symbol.c_str());
+        }
     }
     
     emscripten_fetch_close(fetch);
@@ -64,7 +78,6 @@ void utils::web::web_fetch(const char *url) {
     attr.onsuccess = s_web_fetch_succeeded;
     attr.onerror = s_web_fetch_failed;
     attr.requestHeaders = REQUEST_HEADERS;
-    //attr.userData = this;
     emscripten_fetch(&attr, url);
 }
 
@@ -86,14 +99,32 @@ void utils::web::web_fetch_persist_file_store(const char *url, const void *data,
     emscripten_fetch(&attr, url);
 }
 
-bool utils::web::web_fetch_persist_file_load(const char *url, void *data, size_t size) {
+void utils::web::web_fetch_persist_file_load(const char *url, void *data, size_t size) {
+    PRINT_FUNCTION_NAME();
+
+    emscripten_fetch_attr_t attr;
+    emscripten_fetch_attr_init(&attr);
+
+    auto *user_data = new WebFetchUserData();
+    user_data->data = data;
+    user_data->size = size;
+    
+    strcpy(attr.requestMethod, "GET");
+    attr.attributes = (EMSCRIPTEN_FETCH_LOAD_TO_MEMORY | EMSCRIPTEN_FETCH_PERSIST_FILE);
+    attr.onsuccess = s_web_fetch_succeeded;
+    attr.onerror = s_web_fetch_failed;
+    attr.userData = user_data;
+    emscripten_fetch(&attr, url);
+}
+
+bool utils::web::web_fetch_persist_file_load_sync(const char *url, void *data, size_t size) {
     PRINT_FUNCTION_NAME();
 
     emscripten_fetch_attr_t attr;
     emscripten_fetch_attr_init(&attr);
     
     strcpy(attr.requestMethod, "GET");
-    attr.attributes = (EMSCRIPTEN_FETCH_LOAD_TO_MEMORY | EMSCRIPTEN_FETCH_PERSIST_FILE | EMSCRIPTEN_FETCH_SYNCHRONOUS);
+    attr.attributes = (EMSCRIPTEN_FETCH_LOAD_TO_MEMORY /*| EMSCRIPTEN_FETCH_PERSIST_FILE |*/ EMSCRIPTEN_FETCH_SYNCHRONOUS);
 
     auto ret = false;
     auto *fetch = emscripten_fetch(&attr, url);
--- libraries/app_core/src/impl/platform/web/utils.h
+++ libraries/app_core/src/impl/platform/web/utils.h
@@ -34,7 +34,8 @@ namespace utils {
     namespace web {
         void web_fetch(const char *url);
         void web_fetch_persist_file_store(const char *url, const void *data, size_t size);
-        bool web_fetch_persist_file_load(const char *url, void *data, size_t size);
+        void web_fetch_persist_file_load(const char *url, void *data, size_t size);
+        bool web_fetch_persist_file_load_sync(const char *url, void *data, size_t size);
         void web_fetch_persist_file_delete(const char *url);
     }
 }
