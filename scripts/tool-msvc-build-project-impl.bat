184f7813eef5c1a48a847ad7d555983cb3b2cee5
ignite720 <ignite720@outlook.com>
2025-02-05 22:53:36 +0800

    init12

--- scripts/tool-msvc-build-project-impl.bat
+++ scripts/tool-msvc-build-project-impl.bat
@@ -1,11 +1,11 @@
 @echo off
 
-set G_VSFilePath="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
-set G_VSTarget=%1
-set G_VSSolutionFile=%2
-set G_VSProjectFile=%3
+set MSVS_DEVENV_PATH="C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe"
+set MSVS_TARGET=%1
+set MSVS_SOLUTION_PATH=%2
+set MSVS_PROJECT_PATH=%3
 
-echo %G_VSTarget%
-echo %G_VSSolutionFile%
-echo %G_VSProjectFile%
-call %G_VSFilePath% /Build %G_VSTarget% /ProjectConfig %G_VSTarget% /Project %G_VSProjectFile% %G_VSSolutionFile%
+echo %MSVS_TARGET%
+echo %MSVS_SOLUTION_PATH%
+echo %MSVS_PROJECT_PATH%
+call %MSVS_DEVENV_PATH% /Build %MSVS_TARGET% /ProjectConfig %MSVS_TARGET% /Project %MSVS_PROJECT_PATH% %MSVS_SOLUTION_PATH%
