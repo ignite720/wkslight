fe827dce208de46b210e049b2450c9e22e28eab9
ignite720 <ignite720@outlook.com>
2025-02-27 20:47:13 +0800

    init28

--- scripts/xcode-build.sh
+++ scripts/xcode-build.sh
@@ -1,2 +1,4 @@
 xcodebuild -list -workspace build/wkslight.xcworkspace
-xcodebuild build -workspace build/wkslight.xcworkspace -scheme app -configuration Release
+
+#xcodebuild build -workspace build/wkslight.xcworkspace -scheme app -configuration Release -destination 'platform=macOS,arch=x86_64'
+xcodebuild build -workspace build/wkslight.xcworkspace -scheme app -configuration Release -arch x86_64
