c0779c25d26673483a19e1aedfb62b7000e3d988
ignite720 <ignite720@outlook.com>
2025-04-19 18:48:44 +0800

    test16

--- libraries/app_core/include/app_core/app_core_sdl2.h
+++ libraries/app_core/include/app_core/app_core_sdl2.h
@@ -1,23 +1,17 @@
 #pragma once
 
 #if !TARGET_PLATFORM_ANDROID
-
-#if defined(_WIN32) || defined(__APPLE__)
-
-#include <SDL.h>
-#include <SDL_image.h>
-#include <SDL_mixer.h>
-#include <SDL_net.h>
-#include <SDL_ttf.h>
-
-#else
-
-#include <SDL2/SDL.h>
-#include <SDL2/SDL_image.h>
-#include <SDL2/SDL_mixer.h>
-#include <SDL2/SDL_net.h>
-#include <SDL2/SDL_ttf.h>
-
-#endif
-
+    #if defined(_WIN32) || defined(__APPLE__)
+        #include <SDL.h>
+        #include <SDL_image.h>
+        #include <SDL_mixer.h>
+        #include <SDL_net.h>
+        #include <SDL_ttf.h>
+    #else
+        #include <SDL2/SDL.h>
+        #include <SDL2/SDL_image.h>
+        #include <SDL2/SDL_mixer.h>
+        #include <SDL2/SDL_net.h>
+        #include <SDL2/SDL_ttf.h>
+    #endif
 #endif
