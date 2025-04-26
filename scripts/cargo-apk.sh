2ded936b63be200e7f45af11313f09ea2f275774
ignite720 <ignite720@outlook.com>
2025-04-26 15:58:53 +0800

    test37

--- scripts/cargo-apk.sh
+++ scripts/cargo-apk.sh
@@ -3,7 +3,7 @@ pushd app-rs-android
 THE_SO_DIR=$(find ../build/app/app/.cxx/Debug -type d -path "*/arm64-v8a" | head -n 1)
 echo "THE_SO_DIR=${THE_SO_DIR}"
 ls -lah ${THE_SO_DIR}
-RUSTFLAGS='-L "${THE_SO_DIR}" -lc++_shared -lapp_core_d3d12 -lapp_core_vulkan -lfoo' cargo apk build --profile android-cargo-apk
+RUSTFLAGS="-L ${THE_SO_DIR} -lc++_shared -lapp_core_d3d12 -lapp_core_vulkan -lfoo" cargo apk build --profile android-cargo-apk
 
 popd
 
