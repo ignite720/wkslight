10bce4a1dbd2f0598c86837c036ccf3a6fab4321
ignite720 <ignite720@outlook.com>
2025-05-13 02:57:59 +0800

    test10

--- app/premake5.lua
+++ app/premake5.lua
@@ -72,8 +72,9 @@ project("app")
         })
     filter({ "options:target_platform=pc", "options:pc_deploy_assets" })
         prebuildcommands({
-            "{COPYDIR} %[%{!cfg.debugdir}/assets] %[%{!cfg.targetdir}/assets]",
-            "{MKDIR} %[%{!cfg.targetdir}/assets/shaders]",
+            "{MKDIR} %[%{!cfg.targetdir}/assets]",
+            "{COPYDIR} %{!cfg.debugdir}/assets/* %[%{!cfg.targetdir}/assets]",
+            --"{MKDIR} %[%{!cfg.targetdir}/assets/shaders]",
         })
     filter({ "options:target_platform=pc", "options:pc_deploy_assets", "options:ci", "files:shaders/**.vert", "action:not ninja" })
         buildmessage("Compiling %[%{!file.relpath}], %[%{!file.abspath}]")
