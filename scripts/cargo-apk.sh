2973e66fcc5256d1b63e34681113f539aef6e15c
ignite720 <ignite720@outlook.com>
2025-07-05 19:59:38 +0800

    test42

--- scripts/cargo-apk.sh
+++ scripts/cargo-apk.sh
@@ -1,4 +1,7 @@
-pushd app-rs-android
+APP_RS_ANDROID_DIR=app-rs-android
+BIN_ANDROID_CARGO_APK_DIR=bin/android-cargo-apk
+
+pushd ${APP_RS_ANDROID_DIR}
 
 THE_SO_DIR=$(find ../build/app/app/.cxx/Debug -type d -path "*/arm64-v8a" | head -n 1)
 echo "THE_SO_DIR=${THE_SO_DIR}"
@@ -7,5 +10,5 @@ RUSTFLAGS="-L ${THE_SO_DIR} -lc++_shared -lapp_core_d3d12 -lapp_core_vulkan -lfo
 
 popd
 
-mkdir -p bin/android-cargo-apk
-cp -fv app-rs-android/target/android-cargo-apk/apk/app.apk bin/android-cargo-apk
+mkdir -p ${BIN_ANDROID_CARGO_APK_DIR}
+cp -fv ${APP_RS_ANDROID_DIR}/target/android-cargo-apk/apk/app.apk ${BIN_ANDROID_CARGO_APK_DIR}
--- scripts/cargo-run-pc.sh
+++ scripts/cargo-run-pc.sh
@@ -1,7 +1,7 @@
 APP_RS_DIR=app-rs
 APP_RS_TARGET_RELEASE_DIR=${APP_RS_DIR}/target/release
 
-pushd app-rs
+pushd ${APP_RS_DIR}
 cargo clean
 popd
 
@@ -9,6 +9,6 @@ mkdir -p ${APP_RS_TARGET_RELEASE_DIR}
 cp -rv bin/target/x64/Release/lib*.* $APP_RS_TARGET_RELEASE_DIR
 cp -rv app/assets ${APP_RS_DIR}
 
-pushd app-rs
+pushd ${APP_RS_DIR}
 cargo run -v --release $1
 popd
